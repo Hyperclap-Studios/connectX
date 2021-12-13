a4b619e7b6815c96ae86a97aaa2d1e6ca8c71b8c
Dan Photon <danphoton@yahoo.com>
2021-12-13 03:12:40 +0100

    Setup Routes, Middleware and Classes

---
+++ src/middleware/express/logger.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+
+const logger = (req: express.Request, _res: express.Response, next: express.NextFunction) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+};
+
+export default logger;
